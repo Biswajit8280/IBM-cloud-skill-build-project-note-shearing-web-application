45e3f6a7261e79a7ecb7ac4673cd10240be38825
BISWAJIT BISWAL <152093706+Biswajit8280@users.noreply.github.com>
2024-06-30 10:59:07 +0530

    Add files via upload
    all files has been uploaded.

---
+++ notes/migrations/__init__.py
@@ -0,0 +1,14 @@
+#  run the django admistration "http://127.0.0.1:8000/admin/"
+# pip install django
+# django-admin startproject notes_sharing_app
+
+
+# cd notes_sharing_app
+# python manage.py startapp notes
+# python manage.py makemigrations
+#  python manage.py migrate
+# python manage.py runserver
+
+
+
+# before run the srever must br apply to create superuser= " python manage.py createsuperuser"
